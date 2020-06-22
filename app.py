91dcaa2e6ff4bc9f2696a47f5a55d04eadcf6fba
Raul Mendez <mendezr@tuta.io>
2020-06-21 20:39:13 -0700

    update

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+from flask_frozen import Freezer
 from flask import Flask, render_template
 app = Flask(__name__)
+freezer = Freezer(app)
 
 @app.route("/")
 def default():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
+    freezer.freeze()
+    # app.run()
